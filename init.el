0281034f51de5d108ac0edaaa5fa30886adce09b
Radu Woinaroski <radu.woinaroski@gmail.com>
2017-05-25 17:24:16 +0200

    initial config by RaduW

--- init.el
+++ init.el
@@ -11,15 +11,39 @@
 ;;; Bootstrap use-package
 ;; Install use-package if it's not already installed.
 ;; use-package is used to configure the rest of the packages.
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+;;(unless (package-installed-p 'use-package)
+;;  (package-refresh-contents)
+;;  (package-install 'use-package))
 
-(eval-when-compile
-  (require 'use-package))
-(require 'diminish)                ;; if you use :diminish
-(require 'bind-key)
-(setq use-package-verbose t)
+;;(eval-when-compile
+;;  (require 'use-package))
+;;(require 'diminish)                ;; if you use :diminish
+;;(require 'bind-key)
+;;(setq use-package-verbose t)
 
 ;;; Load the config
-(org-babel-load-file (concat user-emacs-directory "config.org"))
+;;(org-babel-load-file (concat user-emacs-directory "config.org"))
+
+(when window-system
+  (tool-bar-mode -1))
+
+(setq inhibit-startup-message t)
+(setq initial-scratch-message "")
+  
+
+
+(windmove-default-keybindings)
+
+;; initial window
+(setq initial-frame-alist
+      '(
+        (width . 220) ; character
+        (height . 70) ; lines
+        ))
+
+;; default/sebsequent window
+(setq default-frame-alist
+      '(
+        (width . 220) ; character
+        (height . 70) ; lines
+        ))
